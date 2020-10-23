2e84caec8ca7cb69018f1509c0d99b8b99e49713
wafiqohsaadah <72179196+wafiqohsaadah@users.noreply.github.com>
2020-10-23 15:35:13 +0700

    Add files via upload

---
+++ LATIHAN_WAFIQOH/INDEX.js
@@ -0,0 +1,7 @@
+var n = prompt("Masukan nama anda:");
+var c = confirm("Hai "+n+"! Apakah hari ini kamu baca Quran?");
+if (c == true) {
+alert('Semoga tetap Istiqomah!!');
+}else{
+alert('Kenapa?!!');
+}
